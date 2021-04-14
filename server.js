85f8b8753a3d7c7bddfa90572be760d174690a4d
SuhYouri <suhyouri@gmail.com>
2021-04-14 07:24:03 +0100

    finish initial setting

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
-const port = new SerialPort('COM5', {baudRate: 9600})
+const port = new SerialPort('/dev/ttyACM0', {baudRate: 9600})
 const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
 
 const express = require('express')
